a44504f77f88b0c27550442ae64d91d97a45ba18
Veikko Nieminen <veikko.nieminen@tuni.fi>
2022-01-19 17:10:28 +0200

    Add live websocket connection

--- src/components/Live.js
+++ src/components/Live.js
@@ -1,11 +1,53 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
 
 function Live() {
-    return (
-      <div className="live">
-          Live
-      </div>
-    );
-  }
-  
-  export default Live;
-  
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    const ws = new WebSocket("wss://bad-api-assignment.reaktor.com/rps/live");
+
+    const endGame = function(game) {
+      const index = games.findIndex(g => g.gameId === game.gameId);;
+      if (index === -1) {
+        return (games => [...games, game]);
+      } else {
+        const gameCopy = games;
+        gameCopy[index] = game;
+        return gameCopy;
+      }
+    }
+
+    ws.onmessage = function(event) {
+      const parsed = JSON.parse(JSON.parse(event.data));
+
+      if (parsed.type === 'GAME_BEGIN') {
+        setGames(games => [...games, parsed]);
+      } else {
+        setGames(endGame(parsed));
+      }
+    }
+
+    return () => {
+      ws.close();
+    }
+  });
+
+ return (
+  <div className="live">
+      {games.map(game => {
+            return (
+                <>
+                    {game.gameId}
+                    {game.type}
+                    {game.playerA.name}
+                    {game.playerB.name}
+                </>
+            )
+        })}
+  </div>
+);
+}
+
+export default Live;
+
