c0e46bb7634653e12e40222da315f4b6b5324f59
Veikko Nieminen <veikko.nieminen@tuni.fi>
2022-01-21 14:30:46 +0200

    Client history test

--- src/components/History.js
+++ src/components/History.js
@@ -1,8 +1,32 @@
+import React, {useEffect, useState} from 'react'
 
 function History() {
+
+  const [games, setGames] = useState([])
+  const [cursor, setCursor] = useState("/rps/history")
+
+
+  useEffect(() => {
+    //console.log("USEEFGEXTG")
+    console.log(cursor)
+    fetch('https://cors-proxy.nakkivene.dy.fi/https://bad-api-assignment.reaktor.com' + cursor)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+        
+        setGames(games => [...games, ...res.data]);
+        setTimeout(() => {
+          setCursor(res.cursor);
+        }, 500)
+      })
+      .catch(err => alert(err))
+
+  },[cursor]);
+
+
     return (
       <div className="history">
-          History
+          {games.length}
       </div>
     );
   }
